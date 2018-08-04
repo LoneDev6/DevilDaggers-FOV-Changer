321a2e6c3cac641e9558e76b2c5b7643e5a5cda6
lonedev6 <leoxy98@gmail.com>
2018-08-04 15:19:14 +0200

    Finished coding first release

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,77 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.numericUpDown_fov = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_ddOpened = new System.Windows.Forms.Label();
+            this.timer_ddRunning = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_fov)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown_fov
+            // 
+            this.numericUpDown_fov.Location = new System.Drawing.Point(67, 35);
+            this.numericUpDown_fov.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDown_fov.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown_fov.Name = "numericUpDown_fov";
+            this.numericUpDown_fov.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown_fov.TabIndex = 0;
+            this.numericUpDown_fov.ValueChanged += new System.EventHandler(this.numericUpDown_fov_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FOV:";
+            // 
+            // label_ddOpened
+            // 
+            this.label_ddOpened.AutoSize = true;
+            this.label_ddOpened.Location = new System.Drawing.Point(12, 9);
+            this.label_ddOpened.Name = "label_ddOpened";
+            this.label_ddOpened.Size = new System.Drawing.Size(135, 13);
+            this.label_ddOpened.TabIndex = 2;
+            this.label_ddOpened.Text = "DevilDaggers NOT running";
+            // 
+            // timer_ddRunning
+            // 
+            this.timer_ddRunning.Enabled = true;
+            this.timer_ddRunning.Tick += new System.EventHandler(this.timer_ddRunning_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(155, 77);
+            this.Controls.Add(this.label_ddOpened);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown_fov);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_fov)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown_fov;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_ddOpened;
+        private System.Windows.Forms.Timer timer_ddRunning;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Binarysharp.MemoryManagement;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,63 @@ namespace DevilDaggers_FOV_Changer
 {
     public partial class Form1 : Form
     {
+        private string text_ddRunning = "DD is running";
+        private string text_ddNotRunning = "DD NOT running";
+
+
+        private MemorySharp m;
+        private Process dd;
+
+        private static int OFFSET_FOV = 0x14C;
+        private int pFov = -1;
+
         public Form1()
         {
             InitializeComponent();
+            initMemory();
+            if (isDDRunning())
+                numericUpDown_fov.Value = m.Read<int>(new IntPtr(pFov + OFFSET_FOV), false);
+
+            numericUpDown_fov.Enabled = isDDRunning();
+
         }
+
+        private bool isDDRunning() 
+        {
+            return dd != default(Process);
+        }
+
+        private void initMemory()
+        {
+            dd = findProcess("dd");
+            if (dd == null) return;
+            m = new MemorySharp(dd);
+
+            if (pFov == -1)
+                pFov = m.Read<int>(new IntPtr(0x1F4AA0), true);
+        }
+
+        /// <summary>
+        /// Locates a process.
+        /// </summary>
+        /// <param name="ProcessName"></param>
+        /// <returns></returns>
+        private Process findProcess(String processName)
+        {
+            return Process.GetProcessesByName(processName).FirstOrDefault();
+        }
+
+        private void numericUpDown_fov_ValueChanged(object sender, EventArgs e)
+        {
+            if (!isDDRunning()) return;
+            m.Write<int>(new IntPtr(pFov + 0x14C), (int)((NumericUpDown)sender).Value, false);
+        }
+
+        private void timer_ddRunning_Tick(object sender, EventArgs e)
+        {
+            initMemory();
+            label_ddOpened.Text = isDDRunning() ? text_ddRunning : text_ddNotRunning;
+        }
+
     }
 }
